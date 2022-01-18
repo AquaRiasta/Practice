31174dd4e5b2ff2619d509d9796ecdb8115587fe
Nguyen Huu Phuong <huuphuong.aqua@gmail.com>
2022-01-18 14:41:35 +0700

    Add support for ESlint and JSX in FEmasters intro to React

--- reactjs/adopt-me/App.js
+++ reactjs/adopt-me/App.js
@@ -2,11 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet.js";
 
-const App = () =>
-  React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me"),
-    React.createElement(Pet, { name: "Kevin", animal: "Dog", color: "Brown" }),
-    React.createElement(Pet, { name: "Angel", animal: "Bird", color: "Blue" }),
-    React.createElement(Pet, { name: "Sam", animal: "Dog", color: "White" }),
-  ]);
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+// const App = () =>
+//   React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt Me"),
+//     React.createElement(Pet, { name: "Kevin", animal: "Dog", color: "Brown" }),
+//     React.createElement(Pet, { name: "Angel", animal: "Bird", color: "Blue" }),
+//     React.createElement(Pet, { name: "Sam", animal: "Dog", color: "White" }),
+//   ]);
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me</h1>
+      <Pet name="Kevin" animal="Dog" color="White" />
+    </div>
+  )
+};
+
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- reactjs/adopt-me/Pet.js
+++ reactjs/adopt-me/Pet.js
@@ -1,10 +1,20 @@
 import React from "react";
 
-const Pet = (props) =>
-  React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.color),
-  ]);
+// const Pet = (props) =>
+//   React.createElement("div", {}, [
+//     React.createElement("h2", {}, props.name),
+//     React.createElement("h3", {}, props.animal),
+//     React.createElement("h3", {}, props.color),
+//   ]);
+
+const Pet = (props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h2>{props.animal}</h2>
+      <h2>{props.color}</h2>
+    </div>
+  )
+}
 
 export default Pet;
