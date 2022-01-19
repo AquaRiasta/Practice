ad9924bbce6c4c6de223f34286e363686984617c
Nguyen Huu Phuong <huuphuong.aqua@gmail.com>
2022-01-19 16:02:08 +0700

    Finish complete intro to React - FEmasters

--- reactjs/adopt-me/src/App.js
+++ reactjs/adopt-me/src/App.js
@@ -1,17 +1,37 @@
-import React, { StrictMode } from "react";
-import ReactDOM from "react-dom";
-// import Pet from "./Pet.js";
-import SearchParams from "./searchParams.js";
+import { StrictMode, useState } from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const theme = useState("darkblue");
+
   return (
-    <div>
-      <h1>Adopt Me</h1>
-      <SearchParams />
-    </div>
-  )
+    <ThemeContext.Provider value={theme}>
+      <div>
+        <Router>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Switch>
+            <Route path="/details/:id">
+              <Details />
+            </Route>
+            <Route path="/">
+              <SearchParams />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </ThemeContext.Provider>
+  );
 };
 
-
-
-ReactDOM.render(<StrictMode><App /></StrictMode>, document.getElementById("root"));
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
